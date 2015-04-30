5325da47ebd5b350853c5a459b0c389cecd42138
sergigp <sergi.gonzalez@akamon.com>
2015-04-30 15:16:19 +0200

    cool fibonacci with streams

--- fibonacci/src/main/scala/Fibonacci.scala
+++ fibonacci/src/main/scala/Fibonacci.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 
 object Fibonacci {
 
@@ -7,19 +8,20 @@ object Fibonacci {
 //    case _     => calculateNFibonacciSequence(n, List(1, 1))
 //  }
 //
+//  @tailrec
 //  private def calculateNFibonacciSequence(n: Int, acc: List[Int]): List[Int] = n match {
-//    case n if acc.size == n => acc
+//    case _ if acc.size == n => acc
 //    case _          => calculateNFibonacciSequence(n, acc :+ (acc takeRight 2 sum))
 //  }
 
   def get(n: Int): List[Int] = n match {
     case 0     => List()
     case 1 | 2 => List.fill(n)(1)
-    case _     => fib take n toList
+    case _     => fibonacci take n toList
   }
 
-  private def fib(): Stream[Int] = {
-    lazy val fib:Stream[Int] = Stream.cons(1, Stream.cons(1, fib.zip(fib.tail).map(a => a._1 + a._2)))
-    fib
+  def fibonacci(): Stream[Int] = {
+    def fib(a: Int, b: Int): Stream[Int] = a #:: fib(b, a + b)
+    fib(1, 1)
   }
 }
